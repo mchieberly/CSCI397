00bda02fe826f6a26338c3129a434c986937018a
mchieberly <malachieberly@yahoo.com>
2023-11-02 14:36:58 -0400

    Bug fixes

--- Assignment 5/frozen_lake_q_learning_assignment_5.py
+++ Assignment 5/frozen_lake_q_learning_assignment_5.py
@@ -26,7 +26,7 @@ class Agent:
         new_state, reward, terminated, truncated, _ = self.env.step(action)
         old_state = self.state
         if terminated or truncated:
-            new_state = self.env.reset()
+            self.state = self.env.reset()
         return old_state, action, reward, new_state
 
     def best_value_and_action(self, state):
@@ -40,21 +40,21 @@ class Agent:
 
     def value_update(self, state, action, reward, new_state):
         best_new_value, _ = self.best_value_and_action(new_state)
-        new_q_value = reward + (GAMMA * best_new_value)
         state = state[0] if type(state) is tuple else state
-        self.q_table[(state, action)] += ALPHA * (new_q_value - self.q_table[(state, action)])
+        new_q_value = self.q_table[(state, action)] + (ALPHA * (reward + (GAMMA * best_new_value) - self.q_table[(state, action)]))
+        self.q_table[(state, action)] = new_q_value
 
-    def play_episode(self):
+    def play_episode(self, env):
         total_reward = 0.0
-        self.state = self.env.reset()
+        state = env.reset()
         while True:
-            _, action = self.best_value_and_action(self.state)
-            new_state, reward, terminated, truncated, _ = self.env.step(action)
+            _, action = self.best_value_and_action(state)
+            new_state, reward, terminated, truncated, _ = env.step(action)
             total_reward += reward
             if terminated or truncated:
-                self.state = self.env.reset()
+                state = env.reset()
                 break
-            self.state = new_state
+            state = new_state
         return total_reward
 
     def print_values(self):
@@ -86,6 +86,7 @@ class Agent:
         return policy
 
 if __name__ == "__main__":
+    test_env = gym.make(ENV_NAME, desc=None, map_name="4x4", is_slippery=False)
     agent = Agent()
     writer = SummaryWriter(comment="-q-learning")
 
@@ -98,7 +99,7 @@ if __name__ == "__main__":
 
         cumulative_reward = 0.0
         for _ in range(TEST_EPISODES):
-            cumulative_reward += agent.play_episode()
+            cumulative_reward += agent.play_episode(test_env)
         cumulative_reward /= TEST_EPISODES
         writer.add_scalar("reward", cumulative_reward, iter_no)
         if cumulative_reward > best_reward:
